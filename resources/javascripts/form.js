d082978a34e905378d7418e41d33e3b88ad9cd2d
bhavesh732 <bhaveshkhivesra@gmail.com>
2021-04-02 14:18:31 +0530

    carousel speed

--- resources/javascripts/form.js
+++ resources/javascripts/form.js
@@ -5,5 +5,25 @@
      var phone = $("[name='phone']").val();
      var subject = $("[name='subject']").val();
      var message = $("[name='message']").val();
-     $.ajax({ url: "https://docs.google.com/forms/d/e/1FAIpQLSe0-0J7pE8Cv-s247AQpRqerCONFENSajhhRBvRKNyhrHmEHQ/formResponse", data: { "entry.2005620554": fname, "entry.1637468121": lname, "entry.1045781291": email, "entry.1166974658": phone, "entry.1065046570": subject, "entry.839337160": message }, type: "POST", dataType: "xml", statusCode: { 0: function() { alert("THANK YOU! YOUR RESPONSE IS RECORDED SUCESSFULLY."); }, 200: function() { alert("THANK YOU! YOUR RESPONSE IS RECORDED SUCESSFULLY."); } } });
+     $.ajax({
+         url: "https://docs.google.com/forms/d/e/1FAIpQLSfP2EJnbphNq6s-HuAWIr7XHFLUG4oJ9zwto0MEpHSJCdvQeA/formResponse",
+         data: {
+             "entry.227649294": fname,
+             "entry.491522101": lname,
+             "entry.1503863940": email,
+             "entry.842699778": phone,
+             "entry.224739943": subject,
+             "entry.1693096447": message
+         },
+         type: "POST",
+         dataType: "xml",
+         statusCode: {
+             0: function() {
+                 alert("THANK YOU! YOUR RESPONSE IS RECORDED SUCESSFULLY.");
+             },
+             200: function() {
+                 alert("THANK YOU! YOUR RESPONSE IS RECORDED SUCESSFULLY.");
+             }
+         }
+     });
  }
